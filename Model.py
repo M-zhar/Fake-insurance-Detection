3a3ecc4f2e62a21fce096993e2f5db88283be690
Mazhar <mdmazharmm0786@gmail.com>
2025-04-29 14:36:43 +0530

    Added Model File To Initialize the project.
    This Model.py file has initialization of reading dataset.

---
+++ Model.py
@@ -0,0 +1,11 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print(classification_report(y_test, y_pred))
